dbbdad361d8f638ed3ef8dea1c69bf120ca22c7c
Alasdair Davies <a.davies@handshakeproductions.org>
2017-07-07 01:28:43 +0100

    updates

--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# Start sshd if we don't use the init system
+if [ "$INITSYSTEM" != "on" ]; then
+  /usr/sbin/sshd -p 22 &
+fi
+
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+set -e
+
+# Set host-name and enable-dbus
+sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+
+systemctl stop avahi-daemon &&
+
+avahi-daemon
 
 sleep 5
 
